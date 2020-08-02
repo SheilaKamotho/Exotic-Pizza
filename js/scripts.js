18e81a5e936929600f3f18cee9638ca9ab25db82
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-02 04:33:16 +0300

    remove js functions that did not work

--- js/scripts.js
+++ js/scripts.js
@@ -7,24 +7,3 @@ $(document).ready(function(){
 })
 });
 
-function getSizeCost() {
-    let selectedSize = document.getElementById("size").value;
-    return parseInt(selectedSize);
-  }
-
-  function getCrustCost() {
-    let selectedCrust = document.getElementById("crust").value;
-    return parseInt(selectedCrust);
-  }
-
-  function gettoppingsCost() {
-    let selectedCrust = document.getElementById("toppings").value;
-    return parseInt(selectedTopping);
-  }
-
-  function getNumber() {
-    var selectedNumber = document.getElementById("number").value;
-    return parseInt(selectedNumber);
-  }
-
-  
