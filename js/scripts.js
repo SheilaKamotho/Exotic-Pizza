42dc0c6eda8d680f6af37806c595480fcf0e0075
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-02 04:08:53 +0300

    write cost and crust functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
     function () {
     $(this).animate({opacity:'0'});
 })
+
 });
+function getSizeCost() {
+    let selectedSize = document.getElementById("size").value;
+    return parseInt(selectedSize);
+  }
+
+  function getCrustCost() {
+    let selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+  }
