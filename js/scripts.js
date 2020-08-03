86e7c914204f06ad4cdee7122b4b3f3670149bb8
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-03 04:58:22 +0300

    add mathematical functions

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,25 @@ $(document).ready(function() {
             alert('Pizza will be delivered at' + ' ' + location);
         }     
     });
-})    
+
+$('form#pizza').submit(function(event) {
+    var pizzaSize = parseFloat(document.getElementById('size').value);
+    var pizzaCrust = parseFloat(document.getElementById('crust').value);
+    var pizzaTopping = parseFloat(document.getElementById('toppings').value);
+    var pizzaQuantity = parseFloat(document.getElementById('number').value);
+    var pizzaDelivery = parseFloat(document.getElementById('delivery').value);
+    var total = (pizzaSize + pizzaCrust + pizzaTopping + pizzaDelivery)*pizzaQuantity;
+    
+    document.getElementById('displayTotal').innerHTML = total 
+    console.log(total);
+
+    document.getElementById('seeYourSizeChoice').innerHTML = pizzaSize;
+    document.getElementById('seeYourCrustChoice').innerHTML = pizzaCrust;
+    document.getElementById('seeYourToppingsChoice').innerHTML = pizzaTopping;
+    document.getElementById('seeYourQuantityChoice').innerHTML = pizzaQuantity;
+    document.getElementById('seeYourDeliveryChoice').innerHTML = pizzaDelivery;
+    $('#summary').show();
+
+    event.preventDefault();
+});
+})
