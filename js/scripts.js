4233e6cd4607f535a25d9cddb74319c24ab9609f
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-03 04:26:30 +0300

    add hide function

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,19 @@ $(document).ready(function(){
 })
 });
 
+$(document).ready(function() {
+    $('#summary').hide();
+})
+
+$(document).ready(function() {
+    var deliveryPrice = 0;
+    $('#checkout').click(function() {
+        var checkValue = document.getElementById('delivery').value;
+        if (checkValue === 'yes') {
+            deliveryPrice += 200;
+            var location = prompt('Say your location:');
+            alert('Delivery Price is Ksh' + deliveryPrice)
+            alert('pizza will be delivered at' + ' ' + location);
+        }     
+    });
+})    
